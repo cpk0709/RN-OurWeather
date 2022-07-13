85b107ea30b34e1018abdb50680c236f3fd33bfd
cpk0709 <cpk0709@gmail.com>
2022-07-13 23:40:12 +0900

    text edit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello! I made a React Native App!!</Text>
+      <Text>Hello! I made a React Native App!</Text>
       <StatusBar style="auto" />
     </View>
   );
