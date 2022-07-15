f976598396ebb81758dc5649bf2f2191cda397fc
cpk0709 <cpk0709@gmail.com>
2022-07-15 23:39:50 +0900

    ㅅㄷㄴ

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Our Weather(React Native)</Text>
+      <Text>Our Weather</Text>
       <StatusBar style="auto" />
     </View>
   );
